8beb41a92e292d010032cf13cadec13fe9c0c3bb
Mudassir Siddiqui <mudassirsid54@gmail.com>
2025-12-26 16:13:02 +0530

    add prettier

---
+++ impcomment.js
@@ -0,0 +1,6 @@
+// changed it to dev so when you write npm run dev, it runs the index.js file
+//^^^for package.json line 7
+//.prettierrc file created for code formatting preferences using prettier
+//.prettierignore file created to ignore certain files from prettier formatting
+//git init done in terminal to initialize git repository
+//.gitignore file created to ignore node_modules and other files from git tracking
